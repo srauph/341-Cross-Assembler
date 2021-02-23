bd3f5fe780810f985b95ac561968e86d9eac2f12
Mohamed <67757119+CryoSoles@users.noreply.github.com>
2021-02-22 19:23:01 -0500

    removed a useless junit import statement that wasn't being used

--- tests/testPositionClass.java
+++ tests/testPositionClass.java
@@ -1,7 +1,6 @@
 import lexical.Position;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
 
 public class testPositionClass {
 
