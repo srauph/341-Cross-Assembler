39c871e124fb2e2f3e95118b7ec071330dc1dca0
Xaavian Ali <63985071+XaavianAli@users.noreply.github.com>
2021-02-24 01:10:42 -0500

    Grabs file name via options object

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,21 @@ public class Main {
 
 
     public static void main(String[] args) {
-        //Temp way to get file  name, will eventually read from arguments
-        String inputFile = "TestInherentMnemonics.asm";
-        String fileName = inputFile.replace(".asm", "");
+        //Reads filename and arguments
+        Options options = new Options(args);
 
-
-        HashMap<String, Integer> keyWordTable;
         //Will analyze the .asm for tokens
-        Lexical lexical = new Lexical(inputFile);
+        Lexical lexical = new Lexical(options.getInputFile());
 
         //Using the lexical analyzer, parse them to generate a line of statements
         Parser parser = new Parser(lexical, lexical.getKeywords());
         parser.parseTokens();
 
         //Copy over the (IR?) sequential list of line statements to be processed
-        CodeGenerator codeGen = new CodeGenerator(lexical, lexical.getKeywords(), fileName);
+        CodeGenerator codeGen = new CodeGenerator(lexical, lexical.getKeywords(), options.getFileName());
         codeGen.copyIR(parser.getIR());
         codeGen.generateListing();
+
+        System.out.println("Done creating TestInherentMnemonics.lst file.");
     }
 }
