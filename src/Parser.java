d78aad3b46f9371310ea4b177bd11f0299b63dd9
Ali Hanni <55854824+alimus22@users.noreply.github.com>
2021-03-14 18:59:34 -0400

    Parsing Comments ++
    Parse Comments and Instructions. Doesn't handle different addressing modes

--- src/Parser.java
+++ src/Parser.java
@@ -1,8 +1,7 @@
 import ir.IntermediateRep;
 import ir.LineStatement;
 import lexical.LexicalScanner;
-import lexical.token.Token;
-import lexical.token.TokenType;
+import lexical.token.*;
 import utils.SymbolTable;
 
 public class Parser implements IParser {
@@ -35,21 +34,44 @@ public class Parser implements IParser {
         while (nextToken.getType() != TokenType.EOF) {
 //            System.out.println("[Debug]" + this.nextToken);
             // System.out.println("[Debug] Parsing Token " + nextToken.toString());
-            /*
-              Once the token is an EOL, the line statement is finished
-              add it to the linkedlist and create a new line statement object for the next tokens
-              to be read
-             */
-            if (nextToken.getType() == TokenType.EOL) {
-                ir.add(ls);
-                ls = new LineStatement();
-            }
+
             //TODO Check TokenType and create the new objects Instruction, Comment, etc
             /*
              example: if  tokentype == mnemonic, then start creating the Instruction object
                       if it is a comment, make comment object etc
                       Feel free to change this function if it makes it easier for you
              */
+            TokenType type = nextToken.getType();
+            Position position = nextToken.getPosition();
+            String value = nextToken.getValue();
+
+            switch(type) {
+
+                case EOL:               //If token is EOL, LS is finished, add it to IR and start a new one.
+                    System.out.println("[Debug] - " + ls);
+                    ir.add(ls);
+                    ls = new LineStatement();
+                    break;
+
+                case COMMENT:           // If token is a comment
+                    ls.setComment(new Comment(position, value));
+                    break;
+
+                case MNEMONIC:          //If token is a mnemonic
+                    ls.setInstruction(new Instruction(position, value)); // Set instruction
+                    //Set newly created instruction's mnemonic
+                    ls.getInstruction().setMnemonic(new Mnemonic(position, value));
+                    break;
+
+                case OPERAND:
+                    //System.out.println("[Debug] - " + nextToken);
+                    ls.getInstruction().getMnemonic().setOpCode(Integer.parseInt(value)); //Set mnemonic's opcode
+                    Operand operand = new Operand(position, value);
+                    operand.setOperand(Integer.parseInt(value));
+                    ls.getInstruction().setOperand(operand); //set instruction's opcode
+                    break;
+
+            }
 
             //Get the next token to process
             getNextToken();
