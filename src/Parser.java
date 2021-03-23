02a94e10ea580180e141881d00686289b4827624
Isaac Muriuki <isaacmm59@gmail.com>
2021-03-23 19:30:19 -0400

    error reporting edits

--- src/Parser.java
+++ src/Parser.java
@@ -1,28 +1,37 @@
 import ir.IntermediateRep;
 import ir.LineStatement;
 import lexical.LexicalScanner;
-import lexical.token.Mnemonic;
-import lexical.token.Token;
-import lexical.token.TokenType;
+import lexical.token.*;
 import utils.SymbolTable;
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 
 public class Parser implements IParser {
     //Sequence of line statements
     private final IntermediateRep ir = new IntermediateRep();
     // private final LinkedList<ir.LineStatement> intermediateRep = new LinkedList<>();
     private final LexicalScanner lexicalScanner;
-    private final SymbolTable<String, Token> keywords;
+    private final SymbolTable<String, Mnemonic> keywords;
     private Token nextToken;
+    private IErrorReporter errorReporter;
 
-    public Parser(LexicalScanner lexicalScanner, SymbolTable<String, Token> keywords) {
+    public Parser(LexicalScanner lexicalScanner, SymbolTable<String, Mnemonic> keywords, IErrorReporter errorRep) {
         this.lexicalScanner = lexicalScanner;
         this.keywords = keywords;
         this.nextToken = lexicalScanner.getNextToken();
+        this.errorReporter = errorRep;
     }
 
     /**
      * Generates line statements based upon tokens from the lexical analyzer.
      * Currently supports only Mnemomics.
+     *
+     *
+     * Each LineStatement is composed of three optional parts (a label,
+     * an instruction/directive and a comment) followed by an end-of-line (EOL).
+     *
+     * LineStatement = [ Label ] [ Instruction | Directive ] [ Comment ] EOL .
      */
     public void parseTokens() {
         LineStatement ls = new LineStatement();
@@ -30,22 +39,67 @@ public class Parser implements IParser {
         while (nextToken.getType() != TokenType.EOF) {
 //            System.out.println("[Debug]" + this.nextToken);
             // System.out.println("[Debug] Parsing Token " + nextToken.toString());
+
+            //TODO Check TokenType and create the new objects Instruction, Comment, etc
             /*
-              Once the token is an EOL, the line statement is finished
-              add it to the linkedlist and create a new line statement object for the next tokens
-              to be read
+             example: if  tokentype == mnemonic, then start creating the Instruction object
+                      if it is a comment, make comment object etc
+                      Feel free to change this function if it makes it easier for you
              */
-            if (nextToken.getType() == TokenType.EOL) {
-                ir.add(ls);
-                ls = new LineStatement();
-            }
-            // checks if the token is a valid mnemonic keyword
-            else if (keywords.containsKey(nextToken.getName())) {
-                ls.setMnemonic((Mnemonic) nextToken);
+            TokenType type = nextToken.getType();
+            Position position = nextToken.getPosition();
+            String value = nextToken.getValue();
+
+            switch(type) {
+
+                case EOL:               //If token is EOL, LS is finished, add it to IR and start a new one.
+                    //System.out.println("[Debug] - " + ls);
+
+                    if(ls.getInstruction() != null) {
+                        if(ls.getInstruction().getOperand() == null) {
+                            ls.getInstruction().getMnemonic().setMode("inherent");
+                        }
+                    }
+
+                    //Error Reporting
+                    errorReporting(ls);
+                    //System.out.println("[Debug] - Line statement - " + ls.getInstruction());
+                    ir.add(ls);
+                    ls = new LineStatement();
+                    break;
+
+                case COMMENT:           // If token is a comment
+                    ls.setComment(new Comment(position, value));
+                    break;
+
+                case MNEMONIC:          //If token is a mnemonic
+
+                    ls.setInstruction(new Instruction(position, value)); // Set instruction
+                    //Set newly created instruction's mnemonic
+                    ls.getInstruction().setMnemonic(new Mnemonic(position, value));
+                    break;
+
+                case OPERAND:
+
+                    //System.out.println("[Debug] - " + nextToken);
+                    ls.getInstruction().getMnemonic().setOpCode(Integer.parseInt(value)); //Set mnemonic's opcode
+                    Operand operand = new Operand(position, value);
+                    operand.setOperand(Integer.parseInt(value));
+                    ls.getInstruction().setOperand(operand); //set instruction's opcode
+                    ls.getInstruction().getMnemonic().setMode("immediate");
+                    break;
+
+                case UNKNOWN:   // Unknown token
+                    ErrorMsg unknown_token = new ErrorMsg("Unknown token", nextToken.getPosition());
+                    errorReporter.record(unknown_token);
+
             }
 
+            System.out.println(nextToken);
             //Get the next token to process
+
             getNextToken();
+
         }
 //        System.out.println("[Debug]" + this.nextToken);
         //System.out.println("[Debug] Done parsing tokens...");
@@ -54,6 +108,35 @@ public class Parser implements IParser {
 
     public IntermediateRep getIR() {
         return this.ir;
+    }
+
+    /**
+     * Analyzes line statements and report error if one is found.
+     * @param ls
+     * @return
+     */
+    public boolean errorReporting(LineStatement ls) {
+        if (ls.getInstruction()!=null) {//if no instruction then we assume its a line with only a comment and ignore it
+            if (keywords.get(ls.getInstruction().getMnemonic().getValue())==null){ //If Mnemonic not found in symbol table, it is considered invalid
+                System.out.println("Third error");
+                ErrorMsg e1 = new ErrorMsg("Invalid mnemonic or directive.",nextToken.getPosition());
+                errorReporter.record(e1);
+                return true;
+            } else if (keywords.get(ls.getInstruction().getMnemonic().getValue())!=null){
+                if (!keywords.get(ls.getInstruction().getMnemonic().getValue()).getMode().equals("inherent") && ls.getInstruction().getOperand() == null){ //If instruction in not inherent (immediate or relative) but does not have an operand
+                    System.out.println("First error");
+                    ErrorMsg e2 = new ErrorMsg("Instruction requires an operand.",nextToken.getPosition());
+                    errorReporter.record(e2);
+                    return true;
+                }else if (keywords.get(ls.getInstruction().getMnemonic().getValue()).getMode().equals("inherent") && ls.getInstruction().getOperand() != null){ //If instruction is inherent but contains an operand
+                    System.out.println("Second error");
+                    ErrorMsg e3 = new ErrorMsg("Inherent instruction must not have an operand.",nextToken.getPosition());
+                    errorReporter.record(e3);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private void getNextToken() {
